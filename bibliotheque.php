54b991501f49872798a404339e32171b17a7b535
SUN Julie <90616280+JulieSOLEIL@users.noreply.github.com>
2021-12-01 21:47:24 +0100

    Add files via upload
    ajout de nouveaux fichiers et MAJ pour certains

--- bibliotheque.php
+++ bibliotheque.php
@@ -20,34 +20,44 @@ $page = 'bibliotheque';
     <main>
         <div class="contenu">
             <h1>L'histoire de L'INCONNU</h1>
-            <div class="img_biblio01"><img src="img_library02.jpg" alt="bibliothèque"></div>
-            <div class="img_biblio02"><img src="img_library03.jpg" alt="library"></div>
-            <div class="txt_biblio01">
-                <span class="para_biblio"><h3>Le Lieu</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maiores similique quam. Explicabo, voluptatibus deserunt distinctio est, nemo eum nisi minus sapiente quis quas ea incidunt odio aut, aspernatur doloremque.
-                Error nobis et modi voluptates rem minima asperiores similique vel dicta ratione delectus iste quas, veniam nam vero mollitia incidunt perspiciatis fugit doloribus totam ut consequatur tempore cumque cum! Beatae!
-                Maiores qui nesciunt sapiente non provident iusto, incidunt numquam cum ad nisi tempore doloremque voluptates in sequi sunt nihil esse dicta alias voluptatem? Reprehenderit, at. Minus sapiente eius maxime voluptatum.
-                Fugit, laborum fugiat delectus, esse quae accusantium exercitationem earum quo vero recusandae beatae, necessitatibus incidunt ipsum expedita ducimus enim facilis ab? Eligendi odio laborum soluta architecto maxime magni facilis. Ratione.
-                Quas est accusantium excepturi, perferendis nesciunt commodi debitis porro alias molestias dicta, quisquam dolore officia quia eius velit. Recusandae unde totam labore. Quo eum consequuntur accusantium, obcaecati molestias rem eaque?
-                Earum nulla tempora ducimus? Error quas impedit fugit eveniet culpa? Tempore debitis sit, molestiae delectus harum consectetur cum. Adipisci quas perferendis nulla magni quia delectus qui repudiandae voluptates optio possimus?
-                Dolores odio obcaecati ipsa accusantium, at blanditiis aliquam porro sequi corporis ipsam quae recusandae odit sapiente soluta reprehenderit minus quia neque? Voluptatem libero corporis sed quaerat voluptatibus ullam aperiam nemo.
-                Distinctio eligendi vero quisquam assumenda repellat libero numquam iusto placeat iure. Eveniet aperiam iure delectus, repudiandae blanditiis ipsa architecto velit consequatur a? Cumque reiciendis corporis, magnam dolores neque enim! Facere.
-                Veritatis, ut. Minima eius repellendus consectetur beatae consequuntur assumenda, dolores, vitae aperiam dolore adipisci nemo quas. Accusantium debitis ea suscipit sed explicabo maxime nulla voluptatem nobis, temporibus error, magnam quidem!
-                </p></span>
-            </div>
-            <div class="txt_biblio02">
-            <span class="para_biblio"><h3>Le Bâtiment rénové</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maiores similique quam. Explicabo, voluptatibus deserunt distinctio est, nemo eum nisi minus sapiente quis quas ea incidunt odio aut, aspernatur doloremque.
-                Error nobis et modi voluptates rem minima asperiores similique vel dicta ratione delectus iste quas, veniam nam vero mollitia incidunt perspiciatis fugit doloribus totam ut consequatur tempore cumque cum! Beatae!
-                Maiores qui nesciunt sapiente non provident iusto, incidunt numquam cum ad nisi tempore doloremque voluptates in sequi sunt nihil esse dicta alias voluptatem? Reprehenderit, at. Minus sapiente eius maxime voluptatum.
-                Fugit, laborum fugiat delectus, esse quae accusantium exercitationem earum quo vero recusandae beatae, necessitatibus incidunt ipsum expedita ducimus enim facilis ab? Eligendi odio laborum soluta architecto maxime magni facilis. Ratione.
-                Quas est accusantium excepturi, perferendis nesciunt commodi debitis porro alias molestias dicta, quisquam dolore officia quia eius velit. Recusandae unde totam labore. Quo eum consequuntur accusantium, obcaecati molestias rem eaque?
-                Earum nulla tempora ducimus? Error quas impedit fugit eveniet culpa? Tempore debitis sit, molestiae delectus harum consectetur cum. Adipisci quas perferendis nulla magni quia delectus qui repudiandae voluptates optio possimus?
-                Dolores odio obcaecati ipsa accusantium, at blanditiis aliquam porro sequi corporis ipsam quae recusandae odit sapiente soluta reprehenderit minus quia neque? Voluptatem libero corporis sed quaerat voluptatibus ullam aperiam nemo.
-                Distinctio eligendi vero quisquam assumenda repellat libero numquam iusto placeat iure. Eveniet aperiam iure delectus, repudiandae blanditiis ipsa architecto velit consequatur a? Cumque reiciendis corporis, magnam dolores neque enim! Facere.
-                Veritatis, ut. Minima eius repellendus consectetur beatae consequuntur assumenda, dolores, vitae aperiam dolore adipisci nemo quas. Accusantium debitis ea suscipit sed explicabo maxime nulla voluptatem nobis, temporibus error, magnam quidem!
-                </p></span>
-            </div>
+            <section class="section">
+                <div class="partie01">
+                    <div class="img01">
+                    <img class="photo01" src="img_library02.jpg" alt="bibliothèque">
+                    </div>
+                    <div class="para_biblio01">
+                        <h3>Le Lieu</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maiores similique quam. Explicabo, voluptatibus deserunt distinctio est, nemo eum nisi minus sapiente quis quas ea incidunt odio aut, aspernatur doloremque.
+                        Error nobis et modi voluptates rem minima asperiores similique vel dicta ratione delectus iste quas, veniam nam vero mollitia incidunt perspiciatis fugit doloribus totam ut consequatur tempore cumque cum! Beatae!
+                        Maiores qui nesciunt sapiente non provident iusto, incidunt numquam cum ad nisi tempore doloremque voluptates in sequi sunt nihil esse dicta alias voluptatem? Reprehenderit, at. Minus sapiente eius maxime voluptatum.
+                        Fugit, laborum fugiat delectus, esse quae accusantium exercitationem earum quo vero recusandae beatae, necessitatibus incidunt ipsum expedita ducimus enim facilis ab? Eligendi odio laborum soluta architecto maxime magni facilis. Ratione.
+                        Quas est accusantium excepturi, perferendis nesciunt commodi debitis porro alias molestias dicta, quisquam dolore officia quia eius velit. Recusandae unde totam labore. Quo eum consequuntur accusantium, obcaecati molestias rem eaque?
+                        Earum nulla tempora ducimus? Error quas impedit fugit eveniet culpa? Tempore debitis sit, molestiae delectus harum consectetur cum. Adipisci quas perferendis nulla magni quia delectus qui repudiandae voluptates optio possimus?
+                        Dolores odio obcaecati ipsa accusantium, at blanditiis aliquam porro sequi corporis ipsam quae recusandae odit sapiente soluta reprehenderit minus quia neque? Voluptatem libero corporis sed quaerat voluptatibus ullam aperiam nemo.
+                        Distinctio eligendi vero quisquam assumenda repellat libero numquam iusto placeat iure. Eveniet aperiam iure delectus, repudiandae blanditiis ipsa architecto velit consequatur a? Cumque reiciendis corporis, magnam dolores neque enim! Facere.
+                        Veritatis, ut. Minima eius repellendus consectetur beatae consequuntur assumenda, dolores, vitae aperiam dolore adipisci nemo quas. Accusantium debitis ea suscipit sed explicabo maxime nulla voluptatem nobis, temporibus error, magnam quidem!
+                        </p>
+                    </div>
+                </div>
+                <div class="partie02">
+                    <div class="img02">
+                    <img class="photo02" src="img_library03.jpg" alt="library">
+                    </div>
+                    <div class="para_biblio02">
+                    <h3>Le Bâtiment rénové</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maiores similique quam. Explicabo, voluptatibus deserunt distinctio est, nemo eum nisi minus sapiente quis quas ea incidunt odio aut, aspernatur doloremque.
+                    Error nobis et modi voluptates rem minima asperiores similique vel dicta ratione delectus iste quas, veniam nam vero mollitia incidunt perspiciatis fugit doloribus totam ut consequatur tempore cumque cum! Beatae!
+                    Maiores qui nesciunt sapiente non provident iusto, incidunt numquam cum ad nisi tempore doloremque voluptates in sequi sunt nihil esse dicta alias voluptatem? Reprehenderit, at. Minus sapiente eius maxime voluptatum.
+                    Fugit, laborum fugiat delectus, esse quae accusantium exercitationem earum quo vero recusandae beatae, necessitatibus incidunt ipsum expedita ducimus enim facilis ab? Eligendi odio laborum soluta architecto maxime magni facilis. Ratione.
+                    Quas est accusantium excepturi, perferendis nesciunt commodi debitis porro alias molestias dicta, quisquam dolore officia quia eius velit. Recusandae unde totam labore. Quo eum consequuntur accusantium, obcaecati molestias rem eaque?
+                    Earum nulla tempora ducimus? Error quas impedit fugit eveniet culpa? Tempore debitis sit, molestiae delectus harum consectetur cum. Adipisci quas perferendis nulla magni quia delectus qui repudiandae voluptates optio possimus?
+                    Dolores odio obcaecati ipsa accusantium, at blanditiis aliquam porro sequi corporis ipsam quae recusandae odit sapiente soluta reprehenderit minus quia neque? Voluptatem libero corporis sed quaerat voluptatibus ullam aperiam nemo.
+                    Distinctio eligendi vero quisquam assumenda repellat libero numquam iusto placeat iure. Eveniet aperiam iure delectus, repudiandae blanditiis ipsa architecto velit consequatur a? Cumque reiciendis corporis, magnam dolores neque enim! Facere.
+                    Veritatis, ut. Minima eius repellendus consectetur beatae consequuntur assumenda, dolores, vitae aperiam dolore adipisci nemo quas. Accusantium debitis ea suscipit sed explicabo maxime nulla voluptatem nobis, temporibus error, magnam quidem!
+                    </p>
+                    </div>
+                </div>
+            </section>
         </div>  
     </main>
     
